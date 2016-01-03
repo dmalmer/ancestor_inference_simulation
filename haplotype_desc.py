c7b371551e2ec56af5863cedd12a21c6fecc7912
Daniel Malmer <dmalmer@gmail.com>
2016-01-03 09:53:08 -0800

    Clean up

--- haplotype_desc.py
+++ haplotype_desc.py
@@ -35,9 +35,8 @@ next generations will have recombination events:
 '''
 
 
-genome_len = 100
 genomes = [] #index+1 = indv ID
-with open('recombs.log','r') as f:
+with open('data/recombs.log','r') as f:
     cols = f.readline().strip().split()
     first_gen = int(cols[0])
 
@@ -63,10 +62,8 @@ with open('recombs.log','r') as f:
 
     #set rest of genomes
     while len(cols) > 0:
-        #print '\nnewwhile'
         genomes.append({})
         for g_chrom in (0, 1):
-            #print 'cols = ' + str(cols)
             parent = genomes[int(cols[1])-1] 
             curr_c = int(cols[2])
             new_pos = [parent[curr_c][0][0]] #always 0
@@ -75,13 +72,7 @@ with open('recombs.log','r') as f:
             p_i[curr_c] += 1
             for r in cols[3:]:
                 r = int(r)
-                #print r
                 #add anc info from before recomb
-                #print parent[curr_c]
-                #print '\nr = ' + str(r)
-                #print 'p_i = ' + str(p_i)
-                #print 'new_pos = ' + str(new_pos)
-                #print 'new_anc = ' + str(new_anc)
                 while p_i[curr_c] < len(parent[curr_c][0]) and parent[curr_c][0][p_i[curr_c]] < r:
                     if parent[curr_c][0][p_i[curr_c]] > new_pos[-1]:
                         new_pos.append(parent[curr_c][0][p_i[curr_c]])
@@ -90,17 +81,9 @@ with open('recombs.log','r') as f:
                 #add recomb
                 new_pos.append(r)
                 curr_c = abs(curr_c - 1)
-                #print 'parent cols:'
-                #print 'new_pos = ' + str(new_pos)
-                #print 'new_anc = ' + str(new_anc)
-                #print 'p_i = ' + str(p_i)
                 #  catch other chrom parent index to current position 
                 while p_i[curr_c] < len(parent[curr_c][0])-1 and parent[curr_c][0][p_i[curr_c]+1] < r:
-                    #print 'catching up'
                     p_i[curr_c] += 1
-                #print 'parent[curr_c] = ' + str(parent[curr_c])
-                #print 'curr_c = ' + str(curr_c)
-                #print 'p_i = ' + str(p_i)
                 if p_i[curr_c] < len(parent[curr_c][1]):
                     new_anc.append(parent[curr_c][1][p_i[curr_c]])
                 else:
@@ -108,15 +91,9 @@ with open('recombs.log','r') as f:
                     # add the last ancestor
                     new_anc.append(parent[curr_c][1][-1])
 
-                #print 'new_anc = ' + str(new_anc)
-
             #add rest of parent after all recombinations
             new_pos.extend(parent[curr_c][0][p_i[curr_c]+1:])
             new_anc.extend(parent[curr_c][1][p_i[curr_c]+1:])
-                
-            #print len(genomes)
-            #print new_pos
-            #print new_anc
             
             #collapse adjacent positions with the same ancestor and add to genomes list
             genomes[-1][g_chrom] = [[new_pos[0]], [new_anc[0]]]
@@ -125,16 +102,11 @@ with open('recombs.log','r') as f:
                     genomes[-1][g_chrom][0].append(n_p)
                     genomes[-1][g_chrom][1].append(n_a)
 
-            #print '\ngenome ' + str(len(genomes)) + ', g_chrom = ' + str(g_chrom)
-            #print genomes[-1][g_chrom]
             cols = f.readline().strip().split()
 
-        #cols = f.readline().strip().split()
         print '\ngenome ' + str(len(genomes))
         print genomes[-1][0]
         print genomes[-1][1]
-        print ''
-        #break
 
     
 exit(0)
@@ -142,46 +114,5 @@ for i, g in enumerate(genomes):
     print str(i+1) + ':'
     print '   ' + str(g['0'])
     print '   ' + str(g['1'])
-
-'''
-        print '\nnewwhile'
-        genomes.append({})
-        for g_chrom in (0, 1):
-            print 'cols = ' + str(cols)
-            parent = genomes[int(cols[1])-1] 
-            curr_c = int(cols[2])
-            new_pos = [0]
-            new_anc = [parent[curr_c][1][0]]
-            p_i = [0, 0]
-            for r in cols[3:]:
-                r = int(r)
-                print r
-                #add anc info from before recomb
-                print parent[curr_c][0]
-                while p_i[curr_c] < len(parent[curr_c][0]) and parent[curr_c][0][p_i[curr_c]] < r:
-                    new_pos.append(parent[curr_c][0][p_i[curr_c]])
-                    new_anc.append(parent[curr_c][1][p_i[curr_c]])
-                    p_i[curr_c] += 1
-                #add recomb
-                new_pos.append(r)
-                curr_c = abs(curr_c - 1)
-                #  catch other chrom parent index to current position 
-                while p_i[curr_c] < len(parent[curr_c][0]) - 1 and parent[curr_c][0][p_i[curr_c]] < r:
-                    p_i[curr_c] += 1
-                new_anc.append(parent[curr_c][1][p_i[curr_c]])
-                
-            #print len(genomes)
-            #print new_pos
-            #print new_anc
-            genomes[-1][g_chrom] = [new_pos, new_anc] 
-            cols = f.readline().strip().split()
-
-        #cols = f.readline().strip().split()
-        print len(genomes)
-        print genomes[-1][0]
-        print genomes[-1][1]
-        #break
-        '''
-#haplotypes = {i:   for i in range(1, int(lines[0][0]))
 
 
--- sim.py
+++ sim.py
@@ -54,7 +54,7 @@ strains = ('W303', 'YJM555', 'NCIM3186', 'YJM450', 'YJM981', 'S288c', 'YJM451', 
 tot_alignments = 8
 strain_seqs = {i: '' for i in range(tot_alignments)}
 
-with open('kalign-yeast.clustalw', 'r') as f:
+with open('data/kalign-yeast.clustalw', 'r') as f:
     f.readline()
     f.readline()
     f.readline()
@@ -67,8 +67,8 @@ with open('kalign-yeast.clustalw', 'r') as f:
         f.readline()
         line = f.readline().strip()
         j += 1
-        if j > 3:
-            break
+        #if j > 3:
+        #    break
          
 # call SNPs on alignment file and construct reference sequence
 ref_seq = ''
@@ -81,75 +81,76 @@ for i in range(len(strain_seqs[0])):
     for j, s in enumerate(SNPs):
         SNPs_by_strain[j] += s
 
-print ''
-print ref_seq
-for SNPs in SNPs_by_strain.values():
-    print SNPs
-    
-
 
 # create population
-num_loci = 100
-pop = Population(size=2, ploidy=2, loci=num_loci, alleleNames=['A','T','C','G','N'], infoFields=['father_idx', 'mother_idx'])
+#num_loci = 240
+#num_loci = len(strain_seqs[0])
+num_loci = 100000
+pop = Population(size=5, ploidy=2, loci=num_loci, alleleNames=['A','T','C','G','N'], 
+                 #infoFields=['father_idx', 'mother_idx', 'child_idx'])
+                 infoFields=['ind_id'])
 for i in range(0, pop.popSize(), 2):
     pop.individual(i).setSex(MALE)
-    pop.individual(i+1).setSex(FEMALE)
+    if i != pop.popSize()-1:
+        pop.individual(i+1).setSex(FEMALE)
 
 strain_alleles = {}
 for k, v in strain_seqs.items():
     strain_alleles[k] = [int(a) for a in v]
 
-print strain_alleles[0]
+pop.individual(0).setGenotype(strain_alleles[3][:num_loci])
+pop.individual(1).setGenotype(strain_alleles[4][:num_loci])
+pop.individual(2).setGenotype(strain_alleles[5][:num_loci])
+pop.individual(3).setGenotype(strain_alleles[6][:num_loci])
+pop.individual(4).setGenotype(strain_alleles[7][:num_loci])
 
-pop.individual(0).setGenotype(strain_alleles[0][:num_loci])
-pop.individual(1).setGenotype(strain_alleles[2][:num_loci])
+#for i, indv in enumerate(pop.individuals()):
+#    print str(i) + ': ' + str(indv.genotype())
+#    print str(i) + ': ' + str(len(indv.genotype()))
+#    print indv
 
-print '\n\npre evolve:'
-dump(pop)
+#print ''.join([str(n) for n in strain_alleles[0][:num_loci]]).replace('0','A').replace('1','T').replace('2','C').replace('3','G').replace('4','N')
+#print ''.join([str(n) for n in strain_alleles[2][:num_loci]]).replace('0','A').replace('1','T').replace('2','C').replace('3','G').replace('4','N')
+
+#print '\n\npre evolve:'
+#dump(pop)
+
 
 pop.evolve(
-        initOps=sim.InitSex(sex=(sim.MALE, sim.FEMALE)),
+        initOps=[
+            sim.InitSex(sex=(sim.MALE, sim.FEMALE)),
+            sim.IdTagger(),
+            ],
         matingScheme=sim.RandomMating(
             numOffspring=2,
             sexMode=(sim.NUM_OF_MALES, 1),
             ops=[
-                    #sim.Recombinator(rates=.1, convMode=(sim.NUM_MARKERS, 1, 10)),
-                    sim.Recombinator(rates=.01),
                     #sim.ParentsTagger(),
+                    #sim.IdTagger(infoFields=['child_idx']),
+                    sim.IdTagger(),
+                    #sim.Recombinator(rates=.01, output='>test.log', infoFields=['father_idx', 'mother_idx', 'child_idx']),
+                    sim.Recombinator(rates=.0001, output='>>data/recombs.log', infoFields=['ind_id']),
+
                 ]
         ),
-        gen=2
+        gen=10
     )
 
-'''
-pop.evolve(
-        initOps=sim.InitSex(sex=(sim.MALE, sim.FEMALE)),
-        matingScheme=sim.HomoMating(
-        #matingScheme=sim.RandomMating(
-            #sim.SequentialParentChooser(),
-            sim.RandomParentChooser(),
-            sim.OffspringGenerator(
-                ops=[
-                        sim.Recombinator(rates=1, convMode=(sim.NUM_MARKERS, 1, 10)),
-                        #sim.ParentsTagger(),
-                    ],
-                numOffspring=2,
-                sexMode=(sim.NUM_OF_MALES, 1),
-            )
-        ),
-        gen=1
-    )
-'''
-
-#pop.evolve(gen=5)
 
 
-print pop.subPopSize()
+#print pop.subPopSize()
 
-print '\n\npost evolve:'
-dump(pop)
+#print '\n\npost evolve:'
+#dump(pop)
 
-for indv in pop.individuals():
-    print indv.sex()
+for i, indv in enumerate(pop.individuals()):
+    print str(i) + ': ' + str(indv.genotype(chroms=0))
+    print str(i) + ': ' + str(len(indv.genotype(chroms=0)))
+    print indv
 
+print 'strain_alleles'
+print len(strain_alleles[0][:num_loci])
+
+print ref_seq
+print len(ref_seq)
 
