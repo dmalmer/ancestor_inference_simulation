b27fb6553d0e9bccaab87dde8a8daa1ac8d38930
Daniel Malmer <dmalmer@gmail.com>
2016-01-06 17:53:51 -0800

    Output descedant nucleotide sequence

--- haplotype_desc.py
+++ haplotype_desc.py
@@ -34,85 +34,132 @@ next generations will have recombination events:
           [1, 2]]}
 '''
 
+from itertools import tee, izip
 
-genomes = [] #index+1 = indv ID
-with open('data/recombs.log','r') as f:
-    cols = f.readline().strip().split()
-    first_gen = int(cols[0])
+RECOMB_FILE = 'data/recombs_r0.0001_g10.log'
 
-    #set ancestor genomes
-    for a in range(1, first_gen):
-        genomes.append({c: [[0],[a]] for c in (0, 1)})  
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return izip(a, b)
 
-    #set first gen genomes (ancestors are homozygous, so ignore recombs)
-    while int(cols[1]) < first_gen:
-        genomes.append({})
-        genomes[-1][0] = [[0],[int(cols[1])]]  
+if __name__ == '__main__':
+    genomes = [] #index+1 = indv ID
+    with open(RECOMB_FILE,'r') as f:
         cols = f.readline().strip().split()
-        genomes[-1][1] = [[0],[int(cols[1])]]  
-        
-        cols = f.readline().strip().split()
-    
-    print '6:'
-    print genomes[5][0]
-    print genomes[5][1]
-    print '7:'
-    print genomes[6][0]
-    print genomes[6][1]
+        first_gen = int(cols[0])
 
-    #set rest of genomes
-    while len(cols) > 0:
-        genomes.append({})
-        for g_chrom in (0, 1):
-            parent = genomes[int(cols[1])-1] 
-            curr_c = int(cols[2])
-            new_pos = [parent[curr_c][0][0]] #always 0
-            new_anc = [parent[curr_c][1][0]]
-            p_i = [0, 0]
-            p_i[curr_c] += 1
-            for r in cols[3:]:
-                r = int(r)
-                #add anc info from before recomb
-                while p_i[curr_c] < len(parent[curr_c][0]) and parent[curr_c][0][p_i[curr_c]] < r:
-                    if parent[curr_c][0][p_i[curr_c]] > new_pos[-1]:
-                        new_pos.append(parent[curr_c][0][p_i[curr_c]])
-                        new_anc.append(parent[curr_c][1][p_i[curr_c]])
-                    p_i[curr_c] += 1
-                #add recomb
-                new_pos.append(r)
-                curr_c = abs(curr_c - 1)
-                #  catch other chrom parent index to current position 
-                while p_i[curr_c] < len(parent[curr_c][0])-1 and parent[curr_c][0][p_i[curr_c]+1] < r:
-                    p_i[curr_c] += 1
-                if p_i[curr_c] < len(parent[curr_c][1]):
-                    new_anc.append(parent[curr_c][1][p_i[curr_c]])
-                else:
-                    #if the recombination switches to a chrom that is already completely iterated over, we need to simply
-                    # add the last ancestor
-                    new_anc.append(parent[curr_c][1][-1])
+        #set ancestor genomes
+        for a in range(1, first_gen):
+            genomes.append({c: [[0],[a]] for c in (0, 1)})  
 
-            #add rest of parent after all recombinations
-            new_pos.extend(parent[curr_c][0][p_i[curr_c]+1:])
-            new_anc.extend(parent[curr_c][1][p_i[curr_c]+1:])
-            
-            #collapse adjacent positions with the same ancestor and add to genomes list
-            genomes[-1][g_chrom] = [[new_pos[0]], [new_anc[0]]]
-            for n_p, n_a in zip(new_pos[1:], new_anc[1:]):
-                if n_a != genomes[-1][g_chrom][1][-1]:
-                    genomes[-1][g_chrom][0].append(n_p)
-                    genomes[-1][g_chrom][1].append(n_a)
-
+        #set first gen genomes (ancestors are homozygous, so ignore recombs)
+        while int(cols[1]) < first_gen:
+            genomes.append({})
+            genomes[-1][0] = [[0],[int(cols[1])]]  
             cols = f.readline().strip().split()
+            genomes[-1][1] = [[0],[int(cols[1])]]  
+            
+            cols = f.readline().strip().split()
+        
+        #print '6:'
+        #print genomes[5][0]
+        #print genomes[5][1]
+        #print '7:'
+        #print genomes[6][0]
+        #print genomes[6][1]
 
-        print '\ngenome ' + str(len(genomes))
-        print genomes[-1][0]
-        print genomes[-1][1]
+        #set rest of genomes
+        while len(cols) > 0:
+            genomes.append({})
+            for g_chrom in (0, 1):
+                parent = genomes[int(cols[1])-1] 
+                curr_c = int(cols[2])
+                new_pos = [parent[curr_c][0][0]] #always 0
+                new_anc = [parent[curr_c][1][0]]
+                p_i = [0, 0]
+                p_i[curr_c] += 1
+                for r in cols[3:]:
+                    r = int(r)
+                    #add anc info from before recomb
+                    while p_i[curr_c] < len(parent[curr_c][0]) and parent[curr_c][0][p_i[curr_c]] < r:
+                        if parent[curr_c][0][p_i[curr_c]] > new_pos[-1]:
+                            new_pos.append(parent[curr_c][0][p_i[curr_c]])
+                            new_anc.append(parent[curr_c][1][p_i[curr_c]])
+                        p_i[curr_c] += 1
+                    #add recomb
+                    new_pos.append(r)
+                    curr_c = abs(curr_c - 1)
+                    #  catch other chrom parent index to current position 
+                    while p_i[curr_c] < len(parent[curr_c][0])-1 and parent[curr_c][0][p_i[curr_c]+1] < r:
+                        p_i[curr_c] += 1
+                    if p_i[curr_c] < len(parent[curr_c][1]):
+                        new_anc.append(parent[curr_c][1][p_i[curr_c]])
+                    else:
+                        #if the recombination switches to a chrom that is already completely iterated over, we need to simply
+                        # add the last ancestor
+                        new_anc.append(parent[curr_c][1][-1])
 
-    
-exit(0)
-for i, g in enumerate(genomes):
-    print str(i+1) + ':'
-    print '   ' + str(g['0'])
-    print '   ' + str(g['1'])
+                #add rest of parent after all recombinations
+                new_pos.extend(parent[curr_c][0][p_i[curr_c]+1:])
+                new_anc.extend(parent[curr_c][1][p_i[curr_c]+1:])
+                
+                #collapse adjacent positions with the same ancestor and add to genomes list
+                genomes[-1][g_chrom] = [[new_pos[0]], [new_anc[0]]]
+                for n_p, n_a in zip(new_pos[1:], new_anc[1:]):
+                    if n_a != genomes[-1][g_chrom][1][-1]:
+                        genomes[-1][g_chrom][0].append(n_p)
+                        genomes[-1][g_chrom][1].append(n_a)
+
+                cols = f.readline().strip().split()
+
+            print '\ngenome ' + str(len(genomes))
+            print genomes[-1][0]
+            print genomes[-1][1]
+
+    # write out nucleotide seq of final genome
+    # subject to change, but current sim pulls in the following strains:
+    #pop.individual(0).setGenotype(strain_alleles[3][:num_loci])
+    #pop.individual(1).setGenotype(strain_alleles[4][:num_loci])
+    #pop.individual(2).setGenotype(strain_alleles[5][:num_loci])
+    #pop.individual(3).setGenotype(strain_alleles[6][:num_loci])
+    #pop.individual(4).setGenotype(strain_alleles[7][:num_loci])
+    strain_mapping = {1: 3, 2: 4, 3: 5, 4: 6, 5: 7}
+
+    # read alignment file
+    tot_alignments = 8
+    strain_seqs = {i: '' for i in range(tot_alignments)}
+
+    with open('data/kalign-yeast.clustalw', 'r') as f:
+        f.readline()
+        f.readline()
+        f.readline()
+        line = f.readline().strip()
+        while line != '':
+            for i in range(tot_alignments):
+                strain_seqs[i] += line.split()[1]
+                line = f.readline().strip()
+            f.readline()
+            line = f.readline().strip()
+
+    print strain_seqs[3][:100]
+
+    #num_loci = len(strain_seqs[0])
+    num_loci = 100000
+    descendant = ''
+    for (s, e), anc in zip(pairwise(genomes[-1][0][0] + [num_loci]), genomes[-1][0][1]):
+        #print anc
+        #print s
+        #print e
+        #print len(strain_seqs[strain_mapping[anc]][s:e])
+        descendant += strain_seqs[strain_mapping[anc]][s:e]
+    descendant += '-' * (len(strain_seqs[0]) - len(descendant))
+
+    #print len(descendant)
+    with open('data/descendant_seq.nuc', 'w') as f:
+        f.write(descendant + '\n')
+
+
 
 
