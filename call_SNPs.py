0a4f49d9a073972ade7239893cd09a2df6cc5eab
Daniel Malmer <dmalmer@gmail.com>
2016-04-25 08:15:59 -0600

    ignore positions where all ancestors and descendant share same SNP

--- call_SNPs.py
+++ call_SNPs.py
@@ -19,22 +19,21 @@ if __name__ == '__main__':
     with open(wkdir + './data/mouse_fastas/chr%i.fa' % chrom) as f:
         f.readline()
         strain_seqs['ref'] = ''.join(f.readlines()).replace('\n','')
-        print len(strain_seqs['ref'])
 
     for name in strain_names:
         print ' ' + name
         with open(wkdir + './data/mouse_fastas/%s_chr%i.fa' % (name, chrom)) as f:
             f.readline()
             strain_seqs[name] = f.readline().strip()
-            print len(strain_seqs[name])
 
     # create bed file
     print 'creating bed file'
     with open(wkdir + './data/sim_SNPs.bed', 'w') as f:
         for i, ref_nuc in enumerate(strain_seqs['ref']):
             ancs = [name for name in ['DESC'] + sorted(strain_names) if strain_seqs[name][i] != ref_nuc]
-            if len(ancs) > 0:
+            #ignore lines where all ancestors and desc have same SNPs
+            if len(ancs) > 0 and len(ancs) != len(strain_names) + 1:
                 f.write('chr%i\t%i\t%i\t%s\n' % (chrom, i, i+1, '_'.join(ancs)))
-            if i % 100000 == 0:
+            if i % 1000000 == 0:
                 print i
 
