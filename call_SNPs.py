9826ea731aa220d51221f0daac25caf427bd6c49
Daniel Malmer <dmalmer@gmail.com>
2016-05-03 11:41:08 -0600

    Add append_str and genotyping errors

--- call_SNPs.py
+++ call_SNPs.py
@@ -1,17 +1,17 @@
 
+import sys
+
 if __name__ == '__main__':
-    #wkdir = '/scratch/Users/dama9282/simulation/'
-    wkdir = './'
+    wkdir = '/scratch/Users/dama9282/simulation/'
+    #wkdir = './'
+
+    append_str = '_%s' % sys.argv[1] if len(sys.argv) > 1 else ''
 
     strain_names = ['AKRJ', 'AJ', 'BALBcJ', 'C3HHeJ', 'CASTEiJ', 'CBAJ', 'DBA2J', 'LPJ']
-    chrom = 19
+    chrom = 1
 
     strain_seqs = {}
 
-    # read descendant sequence
-    with open(wkdir + './data/desc_seq.nuc') as f:
-        strain_seqs['DESC'] = f.readline().strip()
-    
     # read fasta files
     print 'reading fastas'
     print ' ref'
@@ -26,14 +26,20 @@ if __name__ == '__main__':
             f.readline()
             strain_seqs[name] = f.readline().strip()
 
-    # create bed file
-    print 'creating bed file'
-    with open(wkdir + './data/sim_SNPs.bed', 'w') as f:
-        for i, ref_nuc in enumerate(strain_seqs['ref']):
-            ancs = [name for name in ['DESC'] + sorted(strain_names) if strain_seqs[name][i] != ref_nuc]
-            #ignore lines where all ancestors and desc have same SNPs
-            if len(ancs) > 0 and len(ancs) != len(strain_names) + 1:
-                f.write('chr%i\t%i\t%i\t%s\n' % (chrom, i, i+1, '_'.join(ancs)))
-            if i % 1000000 == 0:
-                print i
+    # call SNPs on files with and without genotyping error applied
+    for err_str in ('', '_err'):
+        # read descendant sequence
+        with open(wkdir + './data/desc_seq%s%s.nuc' % (err_str, append_str)) as f:
+            strain_seqs['DESC'] = f.readline().strip()
+
+        # create bed file
+        print 'creating bed file'
+        with open(wkdir + './data/sim_SNPs%s%s.bed' % (err_str, append_str), 'w') as f:
+            for i, ref_nuc in enumerate(strain_seqs['ref']):
+                ancs = [name for name in ['DESC'] + sorted(strain_names) if strain_seqs[name][i] != ref_nuc]
+                #ignore lines where all ancestors and desc have same SNPs
+                if len(ancs) > 0 and len(ancs) != len(strain_names) + 1:
+                    f.write('chr%i\t%i\t%i\t%s\n' % (chrom, i, i+1, '_'.join(ancs)))
+                if i % 1000000 == 0:
+                    print i
 
